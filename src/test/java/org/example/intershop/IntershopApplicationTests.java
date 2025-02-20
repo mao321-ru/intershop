ed931ce81bda93dba10513291f19c60183ed2a3d
mao321 <mao321-ru@users.noreply.github.com>
2025-02-20 23:24:13 +0400

    [+] Стартовый проект Spring Boot ( Web + JPA + H2, Gradle-Groovy)

---
+++ src/test/java/org/example/intershop/IntershopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.intershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
