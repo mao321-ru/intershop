6cab06dbea55247326b86228cbc4dc15c3f9353e
mao321 <mao321-ru@users.noreply.github.com>
2025-04-21 02:28:20 +0400

    [!] Добавлен отдельный обработчик ошибки AuthorizationDeniedException

--- app/src/main/java/org/example/intershop/controller/GlobalExceptionHandler.java
+++ app/src/main/java/org/example/intershop/controller/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package org.example.intershop.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import reactor.core.publisher.Mono;
@@ -9,6 +11,13 @@ import reactor.core.publisher.Mono;
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    // Стандартный код для Access Denied (добавлен для исключения обработки универсальным обработчиком ниже)
+    @ExceptionHandler( AuthorizationDeniedException.class)
+    public Mono<ResponseEntity<String>> handleException(AuthorizationDeniedException e) {
+        log.info( "HTTP 403: Access Denied");
+        return Mono.just( ResponseEntity.status( HttpStatus.FORBIDDEN).body( "Access Denied"));
+    }
 
     // Более информационное сообщение об ошибке, т.ч. для ошибок платежного сервиса
     @ExceptionHandler( RuntimeException.class)
